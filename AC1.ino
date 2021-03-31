12015e530b14e6fae6a7bb19fe7db1fef57e95fb
naomifreitas <79206033+naomifreitas@users.noreply.github.com>
2021-03-31 20:06:22 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,11 +1,13 @@
-//variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
+const int azul = 7;
 
 bool estadoLedVermelho = false;
+bool estadoLedAzul = false;
+bool estadoLedVerde = false;
 
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
 const int botaoDelay = 100;
 
@@ -13,44 +15,57 @@ void setup()
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  
+
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
-  
+  pinMode(azul, OUTPUT);
+
   Serial.begin(9600);
-	
+
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
+  Serial.println("Grupo: Alpha               ");
 }
 
 void loop()
 {
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
   	Serial.println("botao 1 apertado");
-    ledVermelho();
+    ledVermelho(true);
   	lastDebounceTime1 = millis();
   }
-  
-  if(getTemperatura() > 30){
+  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao2)){
+  	Serial.println("botao 2 apertado");
+    ledVermelho(false);
+  	lastDebounceTime1 = millis();
+  }
+
+  if(getTemperatura() > 15){
     ledAzul(true);
   }else{
-  	ledAzul(falso); 
+  	ledAzul(false); 
   }
-  	
-  delay(10);
+  if(getLuminosidade() > 5){
+    ledVerde(true);
+  }else{
+  	ledVerde(false); 
+  }
+   
+  delay(100);
 }
 
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
+void ledVermelho(bool estado){
+  estadoLedVermelho = estado;
   digitalWrite(vermelho,estadoLedVermelho);
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+ 	estadoLedVerde = estado;
+	digitalWrite(verde,estadoLedVerde);
 }
 void ledAzul(bool estado){
-	digitalWrite(azul,estado);
+    estadoLedAzul = estado;
+	digitalWrite(azul,estadoLedAzul);
+  
 }
 
 int getTemperatura(){
@@ -59,8 +74,9 @@ int getTemperatura(){
   	return temperaturaC;
 } 
 
+
 int getLuminosidade(){
   	int luminosidade;
 	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
   	return luminosidade;
-}
+} 
